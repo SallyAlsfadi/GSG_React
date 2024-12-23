be75fa83193801af6b5ee41b732577df5f60a552
SallyAlsfadi <alsfadisally@gmail.com>
2024-12-23 10:46:36 -0500

    Enhanced form behavior and state handling

---
+++ student-app/src/components/add-form/add-form.component.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { IStudent } from "../../types";
+import CoursesListForm from "../courses-list-form/courses-list-form.component";
+
+const INITIAL_STUDENT = {
+  age: 0,
+  coursesList: [],
+  id: "",
+  isGraduated: false,
+  name: "",
+};
+interface IProps {
+  className?: string;
+  onSubmit: (std: IStudent) => void;
+}
+const addForm = (props: IProps) => {
+  const [student, setStudent] = useState<IStudent>(INITIAL_STUDENT);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleChange = (field: string, value: any) => {
+    setStudent({ ...student, [field]: value });
+  };
+  const handleSubmit = () => {
+    const newStudent: IStudent = { ...student, id: Date.now().toString() };
+    props.onSubmit(newStudent);
+    handleClear();
+  };
+
+  const handleClear = () => {
+    setStudent(INITIAL_STUDENT);
+  };
+
+  const handleCoursesChange = (list: string[]) => {
+    setStudent({ ...student, coursesList: list });
+  };
+  return (
+    <div className={`wrapper ${props.className} ${isOpen ? "open" : "closed"}`}>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <span>&and;</span> : <span>&or;</span>}{" "}
+        {isOpen ? "Hide" : "Show"} Add Form
+      </button>
+      <div className="input">
+        <label htmlFor="name">Student Name: </label>
+        <input
+          id="name"
+          type="text"
+          value={student.name}
+          onChange={(e) => handleChange("name", e.target.value)}
+        />
+      </div>
+      <div className="input">
+        <label htmlFor="age">Student Age: </label>
+        <input
+          id="age"
+          type="number"
+          min={17}
+          max={40}
+          value={student.age}
+          onChange={(e) => handleChange("age", e.target.value)}
+        />
+      </div>
+      <div className="input">
+        <label htmlFor="isGraduated">Is Student Graduated: </label>
+        <input
+          id="isGraduated"
+          type="checkbox"
+          checked={student.isGraduated}
+          onChange={(e) => handleChange("isGraduated", e.target.checked)}
+        />
+      </div>
+      <div>
+        <CoursesListForm
+          value={student.coursesList}
+          onSubmit={handleCoursesChange}
+        />
+      </div>
+      <div className="Actions">
+        <button onClick={handleSubmit}>Submit</button>
+        <button onClick={handleClear}>Clear</button>
+      </div>
+    </div>
+  );
+};
+
+export default addForm;
