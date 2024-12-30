218f2a8df9c9435378dac6b2779f1962b6497a77
SallyAlsfadi <alsfadisally@gmail.com>
2024-12-30 11:56:32 -0500

    change the inline style to css file

--- Calculator_withReact/calculator-app/src/components/Display/Display.tsx
+++ Calculator_withReact/calculator-app/src/components/Display/Display.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Display.css";
+
 interface DisplayProps {
   Expression: string;
   Result: string | number | null;
@@ -6,20 +8,11 @@ interface DisplayProps {
 
 const Display: React.FC<DisplayProps> = ({ Expression, Result }) => {
   return (
-    <div
-      style={{
-        border: "1px solid #ccc",
-        padding: "20px",
-        borderRadius: "8px",
-        marginBottom: "10px",
-        backgroundColor: "#f9f9f9",
-      }}
-    >
-      <div style={{ fontSize: "20px", color: "#333" }}>{Expression || "0"}</div>
-      <div style={{ fontSize: "24px", color: "#007BFF" }}>
-        {Result !== null ? `= ${Result}` : ""}
-      </div>
+    <div className="display-container">
+      <div className="expression">{Expression || "0"}</div>
+      <div className="result">{Result !== null ? `= ${Result}` : ""}</div>
     </div>
   );
 };
+
 export default Display;
